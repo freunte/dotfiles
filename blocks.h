b7437c5372874e3382c511579d04a5c5b1f4085e
Rene Gomez <rene.nunezdcaceres@gmail>
2024-01-23 20:41:37 -0600

    Deleted someblocks directory, added blocks.h file

---
+++ blocks.h
@@ -0,0 +1,18 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	
+    {" ", "cat /sys/class/net/wlp2s0/operstate",                       30,     0},
+
+	{" ", "getbattery",                                                30,     0},
+
+	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+
+	{" ", "getvolume",                                                 1,      0},
+
+	{" ", "date '+%B %d (%A) %R'",					                    60,		0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = "|";
+static unsigned int delimLen = 3;
